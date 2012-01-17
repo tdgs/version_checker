96533ebcaf4c57fe4b14b1fb3b1ea4ab824a4d47
Georgios Avramidis <avramidg@gmail.com>
2012-01-17 22:21:32 +0200

    instruct rspec to focus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'version_checker'
 
 RSpec.configure do |config|
-
+  config.mock_with :rspec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
